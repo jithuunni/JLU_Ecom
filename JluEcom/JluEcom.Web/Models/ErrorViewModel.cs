fa95ec93ac5841f6b99affd8acdb29d896283212
Sreejith Gopinathan <jithappan@gmail.com>
2021-09-21 20:00:33 +0530

    Initial commit

---
+++ JluEcom/JluEcom.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JluEcom.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
